c463316c7b567294429428b463da79dda8768721
thepercival <coendunnink@gmail.com>
2023-02-01 22:21:35 +0100

    update first part

---
+++ domain/Replacement/Repository.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SuperElf\Replacement;
+
+use Doctrine\ORM\EntityRepository;
+use SportsHelpers\Repository as BaseRepository;
+use SuperElf\Replacement as ReplacementBase;
+
+/**
+ * @template-extends EntityRepository<ReplacementBase>
+ */
+class Repository extends EntityRepository
+{
+    /**
+     * @use BaseRepository<ReplacementBase>
+     */
+    use BaseRepository;
+}
