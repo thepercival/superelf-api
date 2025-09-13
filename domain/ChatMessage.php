49855ff6636d5168957a34f9a3cf41cef3340da2
thepercival <cdkdev@pm.me>
2025-09-13 18:06:09 +0200

    remove final from api

--- domain/ChatMessage.php
+++ domain/ChatMessage.php
@@ -8,7 +8,7 @@ use Sports\Poule;
 use SportsHelpers\Identifiable;
 use SuperElf\Pool\User as PoolUser;
 
-final class ChatMessage extends Identifiable
+class ChatMessage extends Identifiable
 {
     protected \DateTimeImmutable $dateTime;
 
